b05d3e9d6cb4ec30988cdce8d246aa891687b994
Hector Vega <linnk99@gmail.com>
2019-12-12 22:27:58 -0500

    Functions added to create deck, shuffle it and deal firts two cards

--- blackjack.js
+++ blackjack.js
@@ -3,13 +3,41 @@
 // by Hector Vega
 //
 
-let deck = [
-    "Ace of Spades",
-    "Two of Spades",
-    "Three of Spades"
-]
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+let values = ['Ace', 'King', 'Queen', 'Jack',
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six', 
+    'Five', 'Four', 'Three', 'Two']
 
-let playerCards = [ deck[0] ,deck[2] ]
+function createDeck() {
+    let deck = []
+
+    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    
+        for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+            deck.push(values[valueIdx] + ' of ' + suits[suitIdx])
+        }
+    }
+    return deck
+}
+
+function getNextCard() {
+    return deck.shift();
+}
+
+let deck = createDeck()
+
+function shuffleDeck(deck) {
+    deck.sort(function(a, b) {return 0.5 - Math.random()})
+}
+
+shuffleDeck(deck)
+
+for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i])
+}
+
+let playerCards = [ getNextCard(), getNextCard() ]
+let crupierCards = [ getNextCard(), getNextCard()]
 
 console.log("Welcome to Blackjack!")
 
