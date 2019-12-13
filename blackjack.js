5aaac993944d05af72ae3ccb88c503fea918d7b1
Hector Vega <linnk99@gmail.com>
2019-12-12 23:25:56 -0500

    Added buttons to work with the game logic

--- blackjack.js
+++ blackjack.js
@@ -8,6 +8,21 @@ let values = ['Ace', 'King', 'Queen', 'Jack',
     'Ten', 'Nine', 'Eight', 'Seven', 'Six', 
     'Five', 'Four', 'Three', 'Two']
 
+let textArea = document.getElementById('text-area');
+let newGameButton = document.getElementById('new-game-button')
+let hitButton = document.getElementById('hit-button')
+let stayButton = document.getElementById('stay-button')
+
+hitButton.style.display = 'none'
+stayButton.style.display = 'none'
+
+newGameButton.addEventListener('click', function() {
+    textArea.innerText = 'Started...'
+    newGameButton.style.display = 'none'
+    hitButton.style.display = 'inline'
+    stayButton.style.display = 'inline'
+})
+
 function createDeck() {
     let deck = []
 
@@ -52,3 +67,14 @@ console.log("Welcome to Blackjack!")
 console.log("You're dealt: ")
 console.log(" " + getCardString(playerCards[0]))
 console.log(" " + getCardString(playerCards[1]))
+
+/* let textArea = document.getElementById('text-area')
+let okButton = document.getElementById('ok-button')
+
+textArea.style.display = 'none'
+
+okButton.addEventListener('click', function() {
+    //textArea.innerText = 'Button clicked!'
+    textArea.style.display = 'block'
+})
+ */
