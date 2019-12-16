984c2651ee6de79ed923c513670d13b4a5a97606
Hector Vega <linnk99@gmail.com>
2019-12-15 21:46:29 -0500

    Logic for winning added

--- blackjack.js
+++ blackjack.js
@@ -10,10 +10,10 @@ let values = ['Ace', 'King', 'Queen', 'Jack',
     'Five', 'Four', 'Three', 'Two']
 
 //DOM variables
-let textArea = document.getElementById('text-area');
-let newGameButton = document.getElementById('new-game-button')
-let hitButton = document.getElementById('hit-button')
-let stayButton = document.getElementById('stay-button')
+let textArea = document.getElementById('text-area'),
+    newGameButton = document.getElementById('new-game-button'),
+    hitButton = document.getElementById('hit-button'),
+    stayButton = document.getElementById('stay-button')
 
 //Game variables
 let gameStarted = false,
@@ -47,9 +47,7 @@ newGameButton.addEventListener('click', function() {
 
 function createDeck() {
     let deck = []
-
     for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-    
         for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
             let card = {
                 suit: suits[suitIdx],
@@ -61,12 +59,25 @@ function createDeck() {
     return deck
 }
 
-function getNextCard() {
-    return deck.shift();
+/* function shuffleDeck(deck) {
+    deck.sort(function(a, b) {return 0.5 - Math.random()})
+} */
+
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let swapIdx = Math.trunc(Math.random() * deck.length)
+        let tmp = deck[swapIdx]
+        deck[swapIdx] = deck[i]
+        deck[i] = tmp
+    }
 }
 
 function getCardString(card) {
     return card.value + ' of ' + card.suit
+}
+
+function getNextCard() {
+    return deck.shift();
 }
 
 function ShowStatus() {
@@ -76,20 +87,34 @@ function ShowStatus() {
     }
 }
 
-let deck = createDeck()
-
-function shuffleDeck(deck) {
-    deck.sort(function(a, b) {return 0.5 - Math.random()})
+let dealerCardString = ''
+for (let i = 0; i < dealerCards.length; i++) {
+    dealerCardString += getCardString(dealerCards[i]) + '\n'
 }
 
-shuffleDeck(deck)
+let playerCardString = ''
+for (let i = 0; i < dealerCards.length; i++) {
+    playerCardString += getCardString(playerCards[i]) + '\n'
+}
 
+updateScores()
 
-let playerCards = [ getNextCard(), getNextCard() ]
-let crupierCards = [ getNextCard(), getNextCard()]
+textArea.innerText = 
+    'Dealer has:\n' +
+    dealerCardString +
+    '(score: ' + dealerScore + ')\n\n' +
 
-console.log("Welcome to Blackjack!")
+    'Player has:\n' +
+    playerCardString +
+    '(score: ' + playerScore + ')\n\n'
 
-console.log("You're dealt: ")
-console.log(" " + getCardString(playerCards[0]))
-console.log(" " + getCardString(playerCards[1]))
+if (gameOver) {
+    if (playerWon) {
+        textArea.innerText += 'YOU WIN!'
+    } else {
+        textArea.innerText += 'DEALER WINS'
+    }
+    newGameButton.style.display = 'inline'
+    hitButton.style.display = 'none'
+    stayButton.style.display = 'none'
+}
